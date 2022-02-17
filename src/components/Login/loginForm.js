8cf10023950bc223b79eef3d112e317253f972e6
ginigo <gabrielinigo99@gmail.com>
2022-02-17 17:37:20 -0300

    utilizando formik en form login

--- src/components/Login/loginForm.js
+++ src/components/Login/loginForm.js
@@ -1,46 +1,79 @@
-import { useLogin } from "../../hooks/useLogin";
+//import { useLogin } from "../../hooks/useLogin";
+import { Field, Formik, Form } from "formik";
 
 export default function LoginForm() {
-  const { handleChange, handleSubmit, user, formValues } = useLogin();
+  //const { handleChange, handleSubmit, user, formValues } = useLogin();
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group text-white">
-          <label className="text-white">Correo electrónico</label>
-          <input
-            type="email"
-            className="form-control"
-            name="email"
-            value={formValues.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label className="text-light">Contraseña</label>
-          <input
-            type="password"
-            className="form-control"
-            name="password"
-            value={formValues.password}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="d-flex justify-content-end my-3">
-          <button
-            type="submit"
-            className="btn btn-light"
-            disabled={user.loading}
-          >
-            Enviar
-          </button>
-        </div>
-      </form>
-      {user.data.error && (
-        <div className="alert alert-danger mt-3 text-center" role="alert">
-          <span>Usser or Password incorrect</span>
-        </div>
-      )}
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validate={(valores) => {
+          let errores = {};
+
+          //validar mail
+          if (!valores.email) {
+            errores.email = "Please input email.";
+          } else if (
+            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+              valores.email
+            )
+          ) {
+            errores.email =
+              "Email can only contain letters, numbers, periods, hyphens and underscores";
+          }
+
+          //validar longitus password
+          if (!valores.password) {
+            errores.password = "Please input password.";
+          } else if (valores.password.length < 5) {
+            errores.password = "Password must be at least 5 characters";
+          }
+          return errores;
+        }}
+        onSubmit={() => {}}
+      >
+        {({ values, touched, errors }) => (
+          <Form>
+            <div className="mb-3">
+              <Field
+                type="text"
+                name="email"
+                placeholder="correo@correo.com"
+                className="form-control"
+                value={values.email}
+              />
+              {touched.email && errors.email && (
+                <div className="alert alert-danger" role="alert">
+                  {errors.email}
+                </div>
+              )}
+            </div>
+            <div className="mb-3">
+              <Field
+                type="password"
+                name="password"
+                placeholder="*******"
+                className="form-control"
+                value={values.password}
+              />
+              {touched.password && errors.password && (
+                <div className="alert alert-danger" role="alert">
+                  {errors.password}
+                </div>
+              )}
+            </div>
+            <div className="d-flex justify-content-end">
+              <button className="btn btn-dark" type="button">
+                Send
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
