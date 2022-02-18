476f8e5207838fb9423ef0c3dcae77df36b50f57
ginigo <gabrielinigo99@gmail.com>
2022-02-18 11:57:58 -0300

    detalles

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,20 +1,26 @@
 import Logo from "../../sponnacular-logo-light.png";
 import LoginForm from "../../components/Login/loginForm";
 import { Link } from "wouter";
+import { Helmet } from "react-helmet";
 
 export default function Login() {
   return (
-    <div className="container-fluid w-100 vh-100 bg-success d-flex justify-content-center">
-      <div className="row d-flex flex-column align-items-center justify-content-center">
-        <div className="col-12">
-          <Link to="/">
-            <img src={Logo} alt="app-logo" className="w-100" />
-          </Link>
-        </div>
-        <div className="col-12">
-          <LoginForm />
+    <>
+      <Helmet>
+        <title>Spoonacular App | Login</title>
+      </Helmet>
+      <div className="container-fluid w-100 vh-100 bg-success d-flex justify-content-center">
+        <div className="row d-flex flex-column align-items-center justify-content-center">
+          <div className="col-12">
+            <Link to="/">
+              <img src={Logo} alt="app-logo" className="w-100" />
+            </Link>
+          </div>
+          <div className="col-12">
+            <LoginForm />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
