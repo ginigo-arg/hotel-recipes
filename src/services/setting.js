22e3e90dcb1b69faa218cc4c3f634e52ff15d260
ginigo <gabrielinigo99@gmail.com>
2022-02-18 12:13:45 -0300

    Actualizando Api para pruebas

--- src/services/setting.js
+++ src/services/setting.js
@@ -1,3 +1,4 @@
 export const URL = "https://api.spoonacular.com";
-export const API_KEY = "b2ddd44c091e4692baadd55877fea254"; //API UNO
+//export const API_KEY = "b2ddd44c091e4692baadd55877fea254"; //API UNO
 //export const API_KEY = "dd9d5658df6a492b9f160d7f50a2b8c1"; //API DOS
+export const API_KEY = "494a1266a2794e6b9a675ec72b08b3db"; //API TRES :)
