015cbba349550444cc27ace0abc2d4b2281eaa26
ginigo <gabrielinigo99@gmail.com>
2022-02-17 22:57:46 -0300

    Validando campo  searchRecipes

--- src/components/FormMenu/FormMenu.js
+++ src/components/FormMenu/FormMenu.js
@@ -55,7 +55,7 @@ export default function FormMenu() {
           setMenu([]);
         }}
       >
-        {({ errors, touched, on }) => (
+        {({ errors, touched }) => (
           <Form>
             <div className="mb-3">
               <label className="form-label">
--- src/components/searchRecipes/searchRecipes.js
+++ src/components/searchRecipes/searchRecipes.js
@@ -2,26 +2,62 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./searchRecipes.css";
+import { Field, Form, Formik } from "formik";
 
 export default function SearchRecipes() {
-  // const [path, pushLocation] = useLocation();
   const navigate = useNavigate();
   const [keyword, setKeyword] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    navigate(`/search/${keyword}`);
-    setKeyword("");
-  };
-  const handleChange = (e) => {
-    setKeyword(e.target.value);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   navigate(`/search/${keyword}`);
+  //   setKeyword("");
+  // };
+  // const handleChange = (e) => {
+  //   setKeyword(e.target.value);
+  // };
 
   return (
     <div className="container my-4">
       <div className="row d-flex justify-content-center">
         <div className="col-xs-12 col-lg-6">
-          <form onSubmit={handleSubmit}>
+          <Formik
+            initialValues={{
+              keyword: "",
+            }}
+            validate={(valores) => {
+              let errores = {};
+              if (valores.keyword.length < 3) {
+                errores.keyword = "Keyword must have at least 3 characters";
+              }
+              return errores;
+            }}
+            onSubmit={(values, { resetForm }) => {
+              const inputKeyword = values.keyword;
+              navigate(`/search/${inputKeyword}`);
+              resetForm();
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <div>
+                  <Field
+                    className="form-control"
+                    type="text"
+                    placeholder="Search Recipes..."
+                    name="keyword"
+                  />
+                </div>
+                {errors.keyword && (
+                  <div className="alert alert-warning" role="alert">
+                    {errors.keyword}
+                  </div>
+                )}
+              </Form>
+            )}
+          </Formik>
+
+          {/* <form onSubmit={handleSubmit}>
             <input
               className="form-control"
               type="text"
@@ -29,7 +65,7 @@ export default function SearchRecipes() {
               placeholder="Search Recipes..."
               value={keyword}
             ></input>
-          </form>
+          </form> */}
         </div>
       </div>
     </div>
